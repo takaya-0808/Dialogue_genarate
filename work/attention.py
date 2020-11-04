b80ad7634aa105748f797599c12b596de7813aa9
e175751 <e175751@ie.u-ryukyu.ac.jp>
2020-11-05 00:12:18 +0900

    attention commit

---
+++ work/attention.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self):
+        pass
+    def call(self):
+        pass
+
+class SelfAttention(MultiHeadAttention):
+
+    def __init__(self):
+        pass
+    
+    def call(self):
+        pass
+
+class Attention(tf.keras.Model):
+
+    def __init__(self, ):
+        
+    def call(self):
+
